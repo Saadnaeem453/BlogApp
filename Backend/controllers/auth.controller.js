fd8ab7a614c18038d66a0d8a4e32a928e2f9ff9f
Saad Naeem <jsaad3663@gmail.com>
2024-03-12 23:13:44 +0500

    Create signin api route

--- Backend/controllers/auth.controller.js
+++ Backend/controllers/auth.controller.js
@@ -2,7 +2,7 @@
 import { User } from "../models/user.model.js"
 import bcryptjs from "bcryptjs"
 import { errorHandler } from "../utils/error.js";
-
+import jwt from "jsonwebtoken";
 export const signup = async (req, res, next) => {
     const { username, email, password } = req.body;
 
@@ -23,4 +23,34 @@ export const signup = async (req, res, next) => {
         next(error)
     }
 
+}
+export const signin = async (req, res, next) => {
+    const { email, password } = req.body
+    if (!email || !password || email === "" || password === "") {
+        next(errorHandler(400, "All feilds are required "))
+    }
+    try {
+        const validUser = await User.findOne({ email })
+        if (!validUser) {
+            return next(errorHandler(404, "Invalid Credientials!!"))
+        }
+        const validPassword = bcryptjs.compareSync(password, validUser.password)
+        if (!validPassword) {
+            return next(errorHandler(404, "Invalid Credientials!!"))
+        }
+
+        const token = jwt.sign(
+            { id: validUser._id }, process.env.JWT_KEY
+        )
+        const { password: pass, ...rest } = validUser._doc;
+
+        res.status(200).cookie("access-cookie", token, {
+            httpOnly: true
+        }).json(rest)
+
+
+
+    } catch (error) {
+        next(error)
+    }
 }
