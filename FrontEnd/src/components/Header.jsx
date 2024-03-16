1ab6965256edef2308df4467725777e5148cdbdf
Saad Naeem <jsaad3663@gmail.com>
2024-03-16 12:54:12 +0500

    Update Header component with User data i.e avatar

--- FrontEnd/src/components/Header.jsx
+++ FrontEnd/src/components/Header.jsx
@@ -1,10 +1,12 @@
-import { Button, Navbar, TextInput } from "flowbite-react"
+import { Avatar, Button, Dropdown, Navbar, TextInput } from "flowbite-react"
 import { Link, useLocation } from "react-router-dom"
 import { AiOutlineSearch } from "react-icons/ai"
 import { FaMoon } from "react-icons/fa"
+import { useSelector } from "react-redux"
 export default function Header() {
     // return the current location of a React component
     const path = useLocation().pathname;
+    const { currentUser } = useSelector(state => state.user)
     console.log("path", path)
     return (
         <Navbar>
@@ -24,13 +26,36 @@ export default function Header() {
                 <Button className="w-12 h-10 hidden sm:inline" color="gray" pill>
                     <FaMoon />
                 </Button>
-                <Link to="/sign-in" >
-                    <Button gradientDuoTone="purpleToBlue" outline>
-                        Sign In
-                    </Button>
-                </Link>
+                {currentUser ? (
+                    <Dropdown arrowIcon="false" inline label={
+                        <Avatar alt="user" img={currentUser.profilePicture} rounded />
+                    }>
+                        <Dropdown.Header>
+                            <span className="block text-sm">@{currentUser.username}</span>
+                            <span className="block text-sm font-medium truncate">{currentUser.email}</span>
+                        </Dropdown.Header>
+                        <Link to="/dashboard?tab=profile">
+                            <Dropdown.Item>
+                                Profile
+                            </Dropdown.Item>
+                        </Link>
+                        <Dropdown.Divider />
+                        <Dropdown.Item>
+                            Sign out
+                        </Dropdown.Item>
+                    </Dropdown>
+
+                ) : (
+                    <Link to="/sign-in" >
+                        <Button gradientDuoTone="purpleToBlue" outline>
+                            Sign In
+                        </Button>
+                    </Link>
+                )
+                }
+
                 <Navbar.Toggle />
-            </div>
+            </div >
             <Navbar.Collapse>
 
                 <Navbar.Link active={path === "/"} as={"div"}>
@@ -46,6 +71,6 @@ export default function Header() {
                 </Navbar.Link>
 
             </Navbar.Collapse>
-        </Navbar>
+        </Navbar >
     )
 }
