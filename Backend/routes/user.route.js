e29f4906c51744defbda59f277c522aa4f4e1969
Saad Naeem <jsaad3663@gmail.com>
2024-03-20 22:54:45 +0500

    add delete user api route

--- Backend/routes/user.route.js
+++ Backend/routes/user.route.js
@@ -1,7 +1,8 @@
 import express from "express";
-import { test, updateUser } from "../controllers/user.controller.js"
+import { test, updateUser, deleteUser } from "../controllers/user.controller.js"
 import { verifyUser } from "../utils/verifyUser.js";
 const router = express.Router();
 router.get("/test", test)
 router.put("/update/:userId", verifyUser, updateUser)
+router.delete("/delete/:userId", verifyUser, deleteUser)
 export default router; 
